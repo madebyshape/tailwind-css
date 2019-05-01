d22d329d91dcaf9dfc00cbffb6b731f19af9120d
Jason Mayo <jason@bymayo.co.uk>
2019-05-01 14:17:22 +0100

    Added start of framework

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+gulp.task(
+   'default',
+   ['watch', 'browser-sync']
+);
+
+gulp.task(
+   'build',
+   ['css', 'js', 'replace', 'imagemin']
+);
+
+
+
+
+
+
+
+gulp.task(
+   'development',
+   function() {
+      sequence('sass', 'javascript', 'font_awesome', 'modernizr');
+   }
+);
+
+/* Production */
+
+gulp.task(
+   'production',
+   function() {
+      sequence(
+         ['development'],
+         ['favicons', 'favicons_html'],
+         'images',
+         ['minify_css', 'minify_javascript'],
+         'critical_css',
+         'rev'
+      );
+   }
+);
+
+/* Default */
+
+gulp.task(
+   'default',
+   function() {
+      sequence('watch', 'development');
+   }
+);
+
+/* ******************************************************************************************************** */
